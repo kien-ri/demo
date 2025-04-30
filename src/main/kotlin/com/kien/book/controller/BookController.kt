332c44a4eeb7ce64ffccb912f6db7555db6962a7
kien.ri <kien.ri@rightcode.co.jp>
2025-04-30 12:10:23 +0900

    一括削除のdocとcode修正

--- src/main/kotlin/com/kien/book/controller/BookController.kt
+++ src/main/kotlin/com/kien/book/controller/BookController.kt
@@ -16,6 +16,7 @@ import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.PutMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -61,8 +62,8 @@ class BookController(private val bookService: BookService) {
     }
 
     @DeleteMapping("/batch")
-    fun deleteBooks(@RequestBody @Valid deleteReq: BooksDelete): ResponseEntity<Void> {
-        bookService.deleteBooks(deleteReq.ids)
+    fun deleteBooks(@RequestParam @Positive ids: List<Long>): ResponseEntity<Void> {
+        bookService.deleteBooks(ids)
         return ResponseEntity.noContent().build()
     }
 
