2ae77327a8c11609e04e0804b61526c88a951186
kien.ri <kien.ri@rightcode.co.jp>
2025-05-20 11:36:40 +0900

    注釈追加

--- src/main/kotlin/com/kien/book/common/util/ValidationUtils.kt
+++ src/main/kotlin/com/kien/book/common/util/ValidationUtils.kt
@@ -3,6 +3,8 @@ package com.kien.book.common.util
 import com.kien.book.common.InvalidParamCustomException
 
 object ValidationUtils {
+
+    // TODO: 引数のerrorMsgが必要ないように
     fun validatePositiveId(id: Long?, fieldName: String, errorMsg: String) {
         if (id != null && id <= 0) {
             throw InvalidParamCustomException(
