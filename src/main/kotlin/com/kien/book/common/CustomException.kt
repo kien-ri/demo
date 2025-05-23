675955e7bef60bae22c3dcc4a0fc9305b386b434
kien.ri <kien.ri@rightcode.co.jp>
2025-05-23 15:07:00 +0900

    一括更新

--- src/main/kotlin/com/kien/book/common/CustomException.kt
+++ src/main/kotlin/com/kien/book/common/CustomException.kt
@@ -2,34 +2,34 @@ package com.kien.book.common
 
 import com.fasterxml.jackson.annotation.JsonIncludeProperties
 
-// JSONに、上のRuntimeExceptionの情報を出力しないようにする
+// JSONに、親クラスのRuntimeExceptionの情報を出力しないようにする
 @JsonIncludeProperties("message")
 open class CustomException(
     override val message: String
 ): RuntimeException(message)
 
-@JsonIncludeProperties("message")
+@JsonIncludeProperties("message", "field", "value")
 class NotFoundCustomException(
     message: String,
     val field: String,
     val value: Any?,
 ): CustomException(message)
 
-@JsonIncludeProperties("message")
+@JsonIncludeProperties("message", "field", "value")
 data class InvalidParamCustomException(
     override val message: String,
     val field: String,
     val value: Any?,
 ): CustomException(message)
 
-@JsonIncludeProperties("message")
+@JsonIncludeProperties("message", "field", "value")
 class NonExistentForeignKeyCustomException(
     message: String,
     val field: String,
     val value: Any?,
 ): CustomException(message)
 
-@JsonIncludeProperties("message")
+@JsonIncludeProperties("message", "field", "value")
 class DuplicateKeyCustomException(
     message: String,
     val field: String,
